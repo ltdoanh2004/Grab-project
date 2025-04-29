3cad17edc46fd483a142ff3f7f264a13bd493c88
duythien2212 <duythien201204@gmail.com>
2025-04-29 20:55:26 +0700

    feat: add suggest all

--- backend/internal/controller/suggest_controller.go
+++ backend/internal/controller/suggest_controller.go
@@ -32,6 +32,7 @@ func (sc *SuggestController) RegisterRoutes(router *gin.Engine) {
 			suggestion.GET("/accommodations", sc.SuggestAccommodations)
 			suggestion.GET("/places", sc.SuggestPlaces)
 			suggestion.GET("/restaurants", sc.SuggestRestaurants)
+			suggestion.GET("/all", sc.SuggestAll)
 		}
 	}
 }
@@ -107,6 +108,32 @@ func (sc *SuggestController) SuggestRestaurants(ctx *gin.Context) {
 	}
 
 	suggestion, err := sc.suggestSerivce.SuggestRestaurants(travelPreference)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, model.NewResponse("Failed to get restaurant suggestions: "+err.Error(), nil))
+		return
+	}
+	ctx.JSON(http.StatusOK, model.NewResponse("Success", suggestion))
+}
+
+// SuggestRestaurants godoc
+// @Summary Suggest restaurants
+// @Description Get restaurant suggestions based on travel preferences
+// @Tags suggest
+// @Accept json
+// @Produce json
+// @Param preference body dto.TravelPreference true "Travel Preferences"
+// @Success 200 {object} model.Response{data=dto.RestaurantsSuggestion}
+// @Failure 400 {object} model.Response
+// @Failure 500 {object} model.Response
+// @Router /api/v1/suggest/restaurants [get]
+func (sc *SuggestController) SuggestAll(ctx *gin.Context) {
+	travelPreference := &dto.TravelPreference{}
+	if err := ctx.ShouldBindJSON(travelPreference); err != nil {
+		ctx.JSON(http.StatusBadRequest, model.NewResponse("Invalid input: "+err.Error(), nil))
+		return
+	}
+
+	suggestion, err := sc.suggestSerivce.SuggestAll(travelPreference)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, model.NewResponse("Failed to get restaurant suggestions: "+err.Error(), nil))
 		return
