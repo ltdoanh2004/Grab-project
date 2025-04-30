8f2140c7db89adf9c1689ca1dae5b03cb343329a
duythien2212 <duythien201204@gmail.com>
2025-04-30 15:03:34 +0700

    change format of trip preference

--- backend/docs/docs.go
+++ backend/docs/docs.go
@@ -1093,6 +1093,23 @@ const docTemplate = `{
                 }
             }
         },
+        "dto.Participant": {
+            "type": "object",
+            "properties": {
+                "adult": {
+                    "type": "integer"
+                },
+                "children": {
+                    "type": "integer"
+                },
+                "ifant": {
+                    "type": "integer"
+                },
+                "pet": {
+                    "type": "integer"
+                }
+            }
+        },
         "dto.PlaceSuggestion": {
             "type": "object",
             "properties": {
@@ -1213,9 +1230,6 @@ const docTemplate = `{
                 "name": {
                     "type": "string"
                 },
-                "num_reviews": {
-                    "type": "integer"
-                },
                 "opening_hours": {
                     "type": "string"
                 },
@@ -1310,25 +1324,22 @@ const docTemplate = `{
                 "budget": {
                     "type": "string"
                 },
-                "duration_days": {
-                    "type": "integer"
-                },
-                "limit": {
-                    "type": "integer"
+                "end_date": {
+                    "type": "string"
                 },
                 "location": {
                     "type": "string"
                 },
-                "places": {
+                "options": {
                     "type": "array",
                     "items": {
                         "type": "string"
                     }
                 },
-                "season": {
-                    "type": "string"
+                "participant": {
+                    "$ref": "#/definitions/dto.Participant"
                 },
-                "travel_style": {
+                "start_date": {
                     "type": "string"
                 }
             }
