2ac79f33a2de64780f214090a687c6bbb1bcc6db
duythien2212 <duythien201204@gmail.com>
2025-05-01 20:24:27 +0700

    fix: fix insert format

--- backend/internal/model/accommodation.go
+++ backend/internal/model/accommodation.go
@@ -4,7 +4,7 @@ package model
 type Accommodation struct {
 	AccommodationID string        `gorm:"type:char(36);primaryKey" json:"accommodation_id"`
 	DestinationID   string        `gorm:"type:char(36);" json:"destination_id"`
-	Name            string        `gorm:"size:100;not null" json:"name"`
+	Name            string        `gorm:"not null" json:"name"`
 	Link            string        `json:"link"`
 	Price           float64       `gorm:"type:decimal(10,2)" json:"price"`
 	TaxInfo         string        `json:"tax_info"`
